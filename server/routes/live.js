abcbb477b0bd6a5d8111df4dafb4ae58110f2566
Vidrimers <vidrimers2@gmail.com>
2026-04-13 18:23:09 +0300

    fix: добавить импорт fs в server/routes/live.js

--- server/routes/live.js
+++ server/routes/live.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import fs from 'fs';
 import { db } from '../database/db.js';
 
 const __filename = fileURLToPath(import.meta.url);
